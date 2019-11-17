a642e1346025b207eea5acc04fe5bf154283c3ad
Ben Wozniak <wozniakbe@msoe.edu>
2019-11-17 16:13:11 -0600

    Made behaviors user specific in database structure, refactored service

--- src/app/services/users/settings.service.ts
+++ src/app/services/users/settings.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import * as firebase from 'firebase';
+import { DocumentSnapshot } from '@angular/fire/firestore';
+import { Setting } from '../../models/setting.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,10 @@ export class SettingsService {
   constructor(private authService: AuthService) {}
 
   async getSetting(name: string) {
+    // Assumption is that settings are always retrieved before set
+    // This way our user object is set. I have run into issues using
+    // async code inside of constructors, which makes the service null
+    // when injected. -bw
     const user: firebase.User = await this.authService.getUser();
     this.currentUser = user;
     this.userProfile = firebase.firestore().doc(`users/${user.uid}`);
