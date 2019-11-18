dfa66be51e5e5c4bcdef8e6c8f0452a3e80b79fa
Ben Wozniak <wozniakbe@msoe.edu>
2019-11-17 18:50:49 -0600

    Added edit mode on behavior detail

--- src/app/pages/behavior-detail/behavior-detail.page.ts
+++ src/app/pages/behavior-detail/behavior-detail.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Behavior } from '../../models/behavior.interface';
 import { BehaviorsService } from '../../services/behaviors/behaviors.service';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-behavior-detail',
@@ -12,15 +14,54 @@ import { Observable } from 'rxjs';
 export class BehaviorDetailPage implements OnInit {
   public behavior: Observable<any>;
   private behaviorId;
+  private isEditMode: boolean;
+  public editBehaviorForm: FormGroup;
+
   constructor(
-    private behaviorsService: BehaviorsService,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public behaviorsService: BehaviorsService,
+    private router: Router,
+    formBuilder: FormBuilder,
     private route: ActivatedRoute
-  ) {}
+  ) {
+    this.editBehaviorForm = formBuilder.group({
+      behaviorName: ['', Validators.required],
+      points: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.behaviorId = this.route.snapshot.paramMap.get('id');
     this.behaviorsService.getBehavior(this.behaviorId).then(behavior => {
       this.behavior = behavior.valueChanges();
     });
+  }
+
+  toggleEditMode () {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  async editBehavior() {
+    const loading = await this.loadingCtrl.create({});
+
+    const behaviorName: string = this.editBehaviorForm.value.behaviorName;
+    const points: number = this.editBehaviorForm.value.points;
+
+    const behavior: Behavior = {
+      id: this.behaviorId,
+      name: behaviorName,
+      points
+    };
+
+    this.behaviorsService.updateBehavior(behavior.id, behavior).then(() => {
+      loading.dismiss().then(() => {
+        this.isEditMode = false;
+      });
+    }, error => {
+      console.error(error);
+    });
+
+    return await loading.present();
   }
 }
